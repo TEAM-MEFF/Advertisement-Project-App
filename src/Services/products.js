d0cc0427ff63052f8c98995e37d10fb2c57c6bd5
Michael Agyemang Prempeh <agyemangmichael555@gmail.com>
2024-10-21 16:53:02 +0000

    pushing hard

--- src/Services/products.js
+++ src/Services/products.js
@@ -1,11 +1,25 @@
 import { apiClient } from "./config";
 
-export const apiGetProducts = async() =>
-apiClient.get("/products");
 
-export const apiGetOneProduct = async(slug) => {
-    return apiClient.get(`products/${slug}`);
-}
+export const apiGetProducts = () =>
+    apiClient.get("/products");
 
-export const apiAddProducts = async(payload) =>
-    apiClient.post("/products", payload);
+// export const apiGetOneProduct = async (slug) => {
+//     return apiClient.get(`products/${slug}`);
+// }
+
+// export const apiAddProducts = async (payload) =>
+//     apiClient.post("/products", payload);
+
+
+// DEFAULT...
+
+// export const apiGetProducts = async() =>
+// apiClient.get("/products");
+
+// export const apiGetOneProduct = async(slug) => {
+//     return apiClient.get(`products/${slug}`);
+// }
+
+// export const apiAddProducts = async(payload) =>
+//     apiClient.post("/products", payload);

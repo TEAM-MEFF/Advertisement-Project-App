885856e4fb4c7d811265921100b2cb161da9ebcc
Michael Agyemang Prempeh <agyemangmichael555@gmail.com>
2024-10-21 17:40:48 +0000

    successful fetch

--- src/components/ApiProductFetch.jsx
+++ src/components/ApiProductFetch.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { apiGetProducts } from '../Services/products';
+import axios from 'axios';
+import ApiGetProduct from './ApiGetProduct';
 
 const ApiProductFetch = () => {
+    const [apiProducts, setApiProducts] = useState([]);
+
+    const getProducts = async () => {
+        const response = await apiGetProducts()
+
+        // axios.get(`${import.meta.env.VITE_BASE_URL}/todos?, limit=0`);
+        setApiProducts(response.data)
+        console.log(response.data)
+    }
+
+    useEffect(() => {
+        getProducts();
+    }, [])
+
     return (
         <div>
-            
+            <h1>ALL PRODUCTS</h1>
+            <div>
+                {
+                    apiProducts.map((ad) => {
+                        return (
+                            <ApiGetProduct
+                                images={ad.images}
+                                productName={ad.productName}
+                                category={ad.category}
+                                price={ad.price}
+                                description={ad.description}
+                            />
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ProductFetch from '../components/ProductFetch'
 import NavbarResponsive from '../components/NavbarResponsive'
+import ApiProductFetch from '../components/apiProductFetch'
 
 const Home = () => {
     return (
         <div>
             <NavbarResponsive />
-            <ProductFetch />
+            <ApiProductFetch />
+            {/* <ProductFetch /> */}
         </div>
     )
 }

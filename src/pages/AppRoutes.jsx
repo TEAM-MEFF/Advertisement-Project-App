d2ed34098356844576dd18ec2dfe1ddccb3ed301
Efuwa Akyere <efuwayorke13@gmail.com>
2024-10-21 16:50:38 +0000

    after

--- src/pages/AppRoutes.jsx
+++ src/pages/AppRoutes.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "../components/Dashboard";
+import Analytics from "./vendor/components/Analytics";
+import Logout from "./vendor/components/Logout";
+import MyAdvert from "./vendor/components/MyAdvert";
+import Orders from "./vendor/components/Orders";
+import PostAd from "./vendor/components/PostAd";
+import Settings from "./dashboard/Settings";
 
 const AppRoutes = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard/>}></Route>
-        <Route path="/Analytics" element={<Dashboard/>}></Route>
-        <Route path="/" element={<Dashboard/>}></Route>
-        <Route path="/" element={<Dashboard/>}></Route>
-        <Route path="/" element={<Dashboard/>}></Route>
-        <Route path="/" element={<Dashboard/>}></Route>
-        <Route path="/" element={<Dashboard/>}></Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Dashboard />}></Route>
+      <Route path="/analytics" element={<Analytics />}></Route>
+      <Route path="/logout" element={<Logout />}></Route>
+      <Route path="/myadvert" element={<MyAdvert />}></Route>
+      <Route path="/orders" element={<Orders />}></Route>
+      <Route path="/postad" element={<PostAd />}></Route>
+      <Route path="/settings" element={<Settings />}></Route>
+    </Routes>
   );
 };
 

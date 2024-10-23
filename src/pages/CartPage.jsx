a6741d442d1ac864be2f29e3de89c93dc439c11d
Efuwa Akyere <efuwayorke13@gmail.com>
2024-10-23 17:27:11 +0000

    Linking signup and login

---
+++ src/pages/CartPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavbarResponsive from '../components/NavbarResponsive'
+import Cart from './Cart'
+
+const CartPage = () => {
+  return (
+    <div>
+        <NavbarResponsive/>
+        <Cart/>
+    </div>
+  )
+}
+
+export default CartPage
